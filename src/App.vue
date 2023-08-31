cf6bcded52546cc70eeedeae80faf5095265d85c
MontageWEB <394504651@qq.com>
2023-08-31 23:18:21 +0800

    安装 element ui 成功

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
-<template>
-  <router-view />
-</template>
-
+<template><router-view /></template>
 <script>
 export default { name: "App" };
 </script>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -3,11 +3,12 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
+import installElementPlus from './plugins/element'
 
 const app = createApp(App);
- 
+
 // 使用路由实例
 app.use(router);
- 
+
 // 挂载应用
 app.mount('#app');
